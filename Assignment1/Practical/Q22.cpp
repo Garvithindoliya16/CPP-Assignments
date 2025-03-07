24a3de92df19eda4edcbb864840a91fea6e75ac6
Garvithindoliya16 <garvithindoliya16@gmail.com>
2025-03-07 14:06:22 +0530

    Update Q22.cpp

--- Assignment1/Practical/Q22.cpp
+++ Assignment1/Practical/Q22.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Base {
+public:
+    virtual void show() {
+        cout << "Base class" << endl;
+    }
+    virtual ~Base() {} // Virtual destructor for safe polymorphic behavior
+};
+
+// Derived class
+class Derived : public Base {
+public:
+    void show() override {
+        cout << "Derived class" << endl;
+    }
+
+    void derivedFunction() {
+        cout << "This is a function specific to the Derived class." << endl;
+    }
+};
+
+int main() {
+    // Creating an object of the Derived class
+    Base* basePtr = new Derived();
+    
+    // Demonstrating dynamic_cast for safe downcasting
+    Derived* derivedPtr = dynamic_cast<Derived*>(basePtr);  // Safe downcast from Base* to Derived*
+
+    if (derivedPtr) {
+        // If the downcast is successful, we can call the derived class methods
+        derivedPtr->show();
+        derivedPtr->derivedFunction();
+    } else {
+        // If the downcast fails, dynamic_cast returns nullptr
+        cout << "Downcasting failed!" << endl;
+    }
+
+    delete basePtr;  // Cleaning up
+    return 0;
+}
 
