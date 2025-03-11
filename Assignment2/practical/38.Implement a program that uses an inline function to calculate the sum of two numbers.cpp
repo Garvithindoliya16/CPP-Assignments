df941cb90768b6ef0e60c813864b5e36a64cea20
Garvit Hiindoliya <garvithindoliya16@gmail.com>
2025-03-11 18:29:04 +0530

    Create 38.Implement a program that uses an inline function to calculate the sum of two numbers.cpp

---
+++ Assignment2/practical/38.Implement a program that uses an inline function to calculate the sum of two numbers.cpp
@@ -0,0 +1,12 @@
+inline int sumof2(int a , int b){
+    return a+b;
+}
+
+int main() {
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+    cout << "Sum of given numbers is = "<<sumof2(x,y)<<endl;
+
+    return 0;
+}
